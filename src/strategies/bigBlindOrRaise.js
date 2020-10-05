bad64cb4b5b2dfd476c6faa79e3fd0634a371aef
Daniel Bartholomae <daniel@bartholomae.name>
2020-10-05 21:29:35 +0200

    feat: fold if we don't have good cards in late rounds

--- src/strategies/bigBlindOrRaise.js
+++ src/strategies/bigBlindOrRaise.js
@@ -1,5 +1,14 @@
 function bigBlindOrRaise(game) {
+  const round = game.round();
   const score = game.me().score();
+
+  if (round > 3) {
+    if (score < 10) {
+      return game.toCall();
+    }
+    return game.toRaiseByBlinds(1);
+  }
+
   if (score <= 5) {
     return 0;
   }
